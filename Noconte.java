3a15462b367be51ae911a8540ecaf86829c241c1
AJAY BONDE <95242410+ajaybonde@users.noreply.github.com>
2022-02-02 20:48:11 +0530

    Add files via upload

---
+++ Noconte.java
@@ -0,0 +1,16 @@
+package com.Ajay;
+
+public class Noconte {
+    public static void main(String[] args) {
+        int a=0;
+        int b=1;
+        int i=0;
+        while ( i<=10) {
+            int sum = a+b;
+            System.out.println(sum);
+            a=b;
+            b++;
+
+        }
+    }
+    }
